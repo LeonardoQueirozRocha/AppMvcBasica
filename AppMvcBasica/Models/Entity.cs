45805819649a79837f1c98d87b8ef23948efda1a
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2021-09-24 19:24:49 -0300

    Aplicação criada utilizando o template do ASP.NET Core

---
+++ AppMvcBasica/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AppMvcBasica.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
